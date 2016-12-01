e4d16e5e278aa9abae9ab8671404fe65093bf00b
Fausto Murillo <fausto.murillo@softtek.com>
2016-12-01 10:21:20 -0800

    Added ui-router
    Added ui-router dependency through bower

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,6 @@
 angular
-    .module('app', ['ui.router']);
-    app.config(function($stateProvider, $urlRouterProvider) {
+    .module('app', ['ui.router'])
+    .config(function($stateProvider, $urlRouterProvider) {
 
         $urlRouterProvider.otherwise('/home');
 
